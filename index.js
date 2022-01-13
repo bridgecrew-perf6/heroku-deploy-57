59329134b74a25066f5dc8c8b4daa9e889e6363a
Matt <86365526+matthewstack@users.noreply.github.com>
2022-01-13 12:16:05 -0500

    fixed code

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ require("dotenv").config();
 const express = require("express");
 
 const server = express();
+server.use(express.json());
 
-server.get("api/users", (req, res) => {
+server.get("/api/users", (req, res) => {
   res.json([
     { id: 1, username: "matt" },
     { id: 2, username: "john" },
